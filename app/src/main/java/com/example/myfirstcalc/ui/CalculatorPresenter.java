941f97ea11a83dc349c21d21d5f067aeaec86f38
PavelKhromovAndrS <pavelkhromov1993@yandex.ru>
2021-10-10 23:23:04 +0300

    implements presenter

---
+++ app/src/main/java/com/example/myfirstcalc/ui/CalculatorPresenter.java
@@ -0,0 +1,103 @@
+package com.example.myfirstcalc.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myfirstcalc.domain.Calculator;
+import com.example.myfirstcalc.domain.Operations;
+
+public class CalculatorPresenter extends AppCompatActivity {
+    Calculator calculator;
+    CalculatorView calculatorView;
+    Operations operation;
+    Double digit1 = 0.0;
+    Double digit2 = 0.0;
+    public static int BASE = 10;
+    Boolean isDotPressed = false;
+    int divider;
+
+    public CalculatorPresenter(Calculator calculator, CalculatorView calculatorView) {
+        this.calculator = calculator;
+        this.calculatorView = calculatorView;
+    }
+
+    public void dotPressed() {
+        if (!isDotPressed) {
+            isDotPressed = true;
+            divider = BASE;
+        }
+    }
+
+    public void keyPressed(String digit) {
+        if (operation == null) {
+            if (!isDotPressed) {
+                digit1 = digit1 * BASE + Double.parseDouble(digit);
+            } else {
+                digit1 = digit1 + Double.parseDouble(digit) / (double) divider;
+                divider *= BASE;
+            }
+            showResult(digit1);
+        }
+        if (operation != null) {
+            if (!isDotPressed) {
+                digit2 = digit2 * BASE + Double.parseDouble(digit);
+            } else {
+                digit2 = (digit2 + (Double.parseDouble(digit))) / divider;
+                divider *= BASE;
+            }
+            showResult(digit2);
+        }
+    }
+
+    public void operationPressed(Operations operations) {
+        isDotPressed = false;
+        if (operations == Operations.ADD) {
+            calculatorView.showResult(showResult(digit1) + "+");
+            operation = Operations.ADD;
+
+        }
+        if (operations == Operations.SUB) {
+            calculatorView.showResult(showResult(digit1) + "-");
+            operation = Operations.SUB;
+
+        }
+        if (operations == Operations.DIV) {
+            calculatorView.showResult(showResult(digit1) + "/");
+            operation = Operations.DIV;
+
+        }
+        if (operations == Operations.MUL) {
+            calculatorView.showResult(showResult(digit1) + "*");
+            operation = Operations.MUL;
+        }
+    }
+
+    public void equalPressed() {
+        Double result = calculator.doCalcOperations(digit1, digit2, operation);
+        showResult(result);
+        digit1 = result;
+        digit2 = 0.0;
+        operation = null;
+    }
+
+    public void deletePressed() {
+        digit1 = 0.0;
+        digit2 = 0.0;
+        operation = null;
+        calculatorView.showResult("");
+        isDotPressed = false;
+    }
+
+    public String showResult(double doubleValue) {
+        long longValue = (long) doubleValue;
+        if (longValue == doubleValue) {
+            calculatorView.showResult(String.valueOf(longValue));
+            return String.valueOf(longValue);
+        } else
+            calculatorView.showResult(String.valueOf(doubleValue));
+        return String.valueOf(doubleValue);
+
+    }
+
+
+
+}
